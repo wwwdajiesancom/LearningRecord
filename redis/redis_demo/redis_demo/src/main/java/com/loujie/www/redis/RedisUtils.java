e2910373cf59a9daad4572ef28e82ff7ac3b8306
loujie <805240128@qq.com>
2017-06-27 18:13:30 +0800

    代码,及配置

--- redis/redis_demo/redis_demo/src/main/java/com/loujie/www/redis/RedisUtils.java
+++ redis/redis_demo/redis_demo/src/main/java/com/loujie/www/redis/RedisUtils.java
@@ -1,5 +1,39 @@
 package com.loujie.www.redis;
 
+import com.loujie.www.utils.ArgsUitls;
+
+import redis.clients.jedis.JedisPool;
+
 public class RedisUtils {
 
+	public static boolean set(String key, String value) {
+		String result = Jedis.getJedis().set(key, value);
+		if ("ok".equalsIgnoreCase(result)) {
+			return true;
+		}
+		return false;
+	}
+	
+	
+
+}
+
+/**
+ * 获取Jedis
+ * 
+ * @author loujie
+ *
+ */
+class Jedis {
+	private static final String redisIp = ArgsUitls.Config.get("redis_ip");
+	private static final Integer redisPort = Integer.parseInt(ArgsUitls.Config.get("redis_port"));
+
+	static JedisPool jedisPool;
+	static {
+		jedisPool = new redis.clients.jedis.JedisPool(redisIp, redisPort);
+	}
+
+	public static redis.clients.jedis.Jedis getJedis() {
+		return jedisPool.getResource();
+	}
 }
--- redis/redis_demo/redis_demo/src/test/java/com/loujie/www/test/ReidsDemo.java
+++ redis/redis_demo/redis_demo/src/test/java/com/loujie/www/test/ReidsDemo.java
@@ -2,6 +2,7 @@ package com.loujie.www.test;
 
 import org.junit.Test;
 
+import com.loujie.www.redis.RedisUtils;
 import com.loujie.www.utils.ArgsUitls;
 
 import redis.clients.jedis.Jedis;
@@ -32,17 +33,12 @@ public class ReidsDemo {
 		};
 		jedis.subscribe(jps, "c1");
 	}
-	
+
 	@Test
 	public void ping3() {
-		Jedis jedis = new Jedis(ArgsUitls.Config.get("redis_ip"), Integer.parseInt(ArgsUitls.Config.get("redis_port")));
-		JedisPubSub jps = new JedisPubSub() {
-			public void onMessage(String channel, String message) {
-				System.err.println(channel);
-				System.err.println("2:" + message);
-			}
-		};
-		jedis.subscribe(jps, "c1");
+		RedisUtils.set("abc", "jiege");
 	}
-
+	
+	
+	
 }
