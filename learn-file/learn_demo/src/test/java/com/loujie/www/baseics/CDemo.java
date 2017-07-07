c519256953f237a999ca97f41c474e1dfbc3ae1f
loujie <805240128@qq.com>
2017-07-07 15:14:06 +0800

    学习基础

--- learn-file/learn_demo/src/test/java/com/loujie/www/baseics/CDemo.java
+++ learn-file/learn_demo/src/test/java/com/loujie/www/baseics/CDemo.java
@@ -1,39 +1,27 @@
 package com.loujie.www.baseics;
 
-import java.text.ParseException;
-
 import org.junit.Test;
 
 public class CDemo {
 
 	@Test
-	public void ll() throws ParseException {
-		
+	public void lll() {
+		System.err.println("oxoxoxoxooxoxoxoxoxoxo");
 	}
 
 }
 
-class Ni {
-	public static String abc = getAbc();
+class B {
 
-	public static String getAbc() {
-		System.err.println("getAbc()");
-		System.err.println(Ix.abc);
-		return "ooxx";
+	public final static int v = getV();
+
+	public static int getV() {
+		System.err.println("getV");
+		return 3;
 	}
 
-	public void sys() {
-		System.err.println("jsdlkfjdlsfjklsdjfkl");
-	}
-
-	static class Ix {
-		public static final String abc = getOOxx();
-
-		public static String getOOxx() {
-			System.err.println("GETooXX");
-			return "OOxx";
-		}
-
+	static {
+		System.err.println("BBBBB");
 	}
 
 }
